b42f1535eb8f7ea25cf6f74614cb7c5fa6b17743
Versilis Tyson <VersilisTyson@gmail.com>
2019-08-06 17:05:50 -0400

    added logs for activities and notes

--- app/src/main/java/com/lambdaschool/favoritepicturesgallery/DetailsActivity.kt
+++ app/src/main/java/com/lambdaschool/favoritepicturesgallery/DetailsActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.EditText
@@ -17,6 +18,7 @@ class DetailsActivity : AppCompatActivity() {
     private var data: ImageData? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifeCycle", "${javaClass.simpleName} onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
@@ -30,12 +32,13 @@ class DetailsActivity : AppCompatActivity() {
             val intent = Intent(context, FullscreenActivity::class.java)
             intent.putExtra("image", data?.fileUriString)
             startActivity(intent)
-        }
+        } //set on clck listener if pressed on image to display Fullscreen activity
 
     }
 
     override fun onStart() {
         super.onStart()
+        Log.i("LifeCycle", "${javaClass.simpleName} onStart")
 
         image.setImageURI(data?.fileUri)
         //        image.setImageDrawable(getResources().getDrawable(android.R.drawable.btn_star));
@@ -44,9 +47,10 @@ class DetailsActivity : AppCompatActivity() {
         text_uri.text = data?.fileUri.toString()
         text_description.text = data?.description
         edit_description.setText(data?.description)
-    }
+    } //sets information and details for selected image
 
     override fun onBackPressed() {
+        Log.i("LifeCycle", "${javaClass.simpleName} onBackPressed")
         data?.name = edit_name.text.toString()
         data?.description = edit_description.text.toString()
         val resultIntent = Intent()
